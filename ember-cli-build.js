04a183e7027ed89dcff566ca2f01e7d0d09dc1b9
Steve Szczecina <steve@envoy.com>
2019-08-21 21:33:03 -0600

    config fix for deploy

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,10 +1,19 @@
-'use strict';
+"use strict";
 
-const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+const EmberAddon = require("ember-cli/lib/broccoli/ember-addon");
 
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
     // Add options here
+
+    //https://github.com/ember-cli/ember-cli/issues/8075
+    "ember-cli-uglify": {
+      uglify: {
+        compress: {
+          collapse_vars: false
+        }
+      }
+    }
   });
 
   /*
